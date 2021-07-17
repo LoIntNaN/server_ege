345253d5c311a00bee669f7f323f630accea1864
LoIntNaN <58365433+LoIntNaN@users.noreply.github.com>
2021-07-17 16:49:12 +0300

    Add files via upload

---
+++ BOT_ROLES.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+bot = commands.Bot(command_prefix = '!')
+
+
+@bot.command(pass_context = True)
+async def ping(ctx):
+     role = ctx.guild.get_role(863103946063478824) 
+     await ctx.author.add_roles(role, reason=None)
+     await asyncio.sleep(7200)
+     await ctx.author.remove_roles(role, reason=None)
+
+token = os.environ.get("BOT_TOKEN")
+
+
+
+
